5147d2ddefcd2c966325035fadd7e44b82de119a
mertsal61 <mertsal92@gmail.com>
2022-07-28 09:47:22 +0300

    işyeripc

---
+++ class kullanımı/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_kullanımı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            araba araba1 = new araba();
+
+            araba1.kapiSayisii = 4;
+            araba1.arabaRengi = "Kırmızı";
+            araba1.arabaModel = "Renault";
+
+            araba1.kapilarikilitle();
+            araba1.motorlistir();
+
+            Console.WriteLine("Arabanın kapı sayısı:"+araba1.kapiSayisi);
+            Console.WriteLine("Araba Rengi"+araba1.arabaRengi);
+            Console.WriteLine("Arabanın mdoeli"+araba1.arabaModel);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ class kullanımı/araba.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_kullanımı
+{
+    public  class araba
+    {
+        public int kapiSayisi;
+        public string arabaModel;
+
+        public string arabaRengi;
+
+
+        public void motorlistir()
+        {
+            Console.WriteLine("Motor çalışıyor");
+        }
+
+        public void kapilarikilitle()
+        {
+            Console.WriteLine("Kapılar Kilitleniyor..");
+
+            
+        }
+    }
+}
