e4b37d428ef727fb2a24cd3977b1a7c624588883
mertsal61 <mertsal92@gmail.com>
2022-08-01 13:59:24 +0300

    string/değişkenler videosu

---
+++ deneme/ornek.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace deneme
+{
+    public partial class ornek : Form
+    {
+        public ornek()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string kayit;
+            kayit = textBox1.Text +""+ textBox2.Text +""+ textBox4.Text +"" +""+ textBox5.Text;
+            listBox1.Items.Add( kayit);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          //  listBox1.Items.Add(textBox1.Text+textBox2.Text+textBox4.Text+textBox5.Text);
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+            //en az 10 yaşında olsun nasıl yazdırılır.
+        }
+    }
+}
